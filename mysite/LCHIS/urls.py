e126e61d94c0cb8ef7c750496d9b303e79f6e7cf
Irish John De Roxas <irishderoxas@gmail.com>
2024-08-26 18:29:02 +0800

    ui change

--- mysite/LCHIS/urls.py
+++ mysite/LCHIS/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    path("admin/", views.admin_dashboard, name="admin_dashboard"),
+    path("admin/dashboard", views.admin_dashboard, name="admin_dashboard"),
     path("admin/child", views.child_list, name="child_list"),
     path("admin/child_detail/child_id=<int:child_id>/child_form_count=<int:child_form_count>", views.child_detail, name="child_detail"),
     path("admin/child_detail/", views.child_detail, name="child_detail"),
